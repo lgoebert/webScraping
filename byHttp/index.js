96f7e7bdd71d492c02b74806893e767553e5069c
lgoebert <lukas_goebert@web.de>
2021-03-21 14:12:52 +0100

    removed setInterval in index.js

--- byHttp/index.js
+++ byHttp/index.js
@@ -7,19 +7,17 @@ console.log("Starting now");
 
 // convert date to a string in UTC timezone format:
 utils.currentDateTime();
-setInterval(
-    async function(urls) {
-        var len = Object.keys(urls).length;
-        for (var i = 0; i < len; i++) {
-            console.log("-------------");
-            let item_name = await Object.keys(urls)[i];
-            item_name = await getFile(item_name);
-            url = await Object.values(urls)[i];
-            console.log("host: " + url.split("/")[2]);
-            console.log("name: " + item_name);
-            await startReq(url, item_name);
-        }
-    },
-    30 * 60 * 1000,
-    urls_list
-);
+
+async function init(urls) {
+    var len = Object.keys(urls).length;
+    for (var i = 0; i < len; i++) {
+        console.log("-------------");
+        let item_name = await Object.keys(urls)[i];
+        item_name = await getFile(item_name);
+        url = await Object.values(urls)[i];
+        console.log("host: " + url.split("/")[2]);
+        console.log("name: " + item_name);
+        await startReq(url, item_name);
+    }
+}
+init(urls_list);

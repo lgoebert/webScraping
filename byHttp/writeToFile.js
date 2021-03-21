b33eea08617e346a53f2df0317daffccdf4df524
lgoebert <lukas_goebert@web.de>
2021-03-21 01:22:31 +0100

    disabled  lowest_price adding to csv

--- byHttp/writeToFile.js
+++ byHttp/writeToFile.js
@@ -21,20 +21,25 @@ async function writeToCSV(obj, csvPath) {
     } catch (error) {
         console.error("Some data undefined:  " + error.message);
     }
-    CSVToJSON()
-        .fromFile(csvPath)
-        .then((source) => {
-            source.push({
-                date: timestamp,
-                lowest_price: obj.lowest_price,
-                median_price: obj.median_price,
-                volume: obj.volume,
+    if (obj.lowest_price == "") {
+        console.log("Price not available. Returning.");
+        return;
+    } else {
+        CSVToJSON()
+            .fromFile(csvPath)
+            .then((source) => {
+                source.push({
+                    date: timestamp,
+                    lowest_price: obj.lowest_price,
+                    median_price: obj.median_price,
+                    volume: obj.volume,
+                });
+                var csv = JSONToCSV(source, {
+                    fields: ["date", "lowest_price", "median_price", "volume"],
+                });
+                FileSystem.writeFileSync(csvPath, csv);
             });
-            var csv = JSONToCSV(source, {
-                fields: ["date", "lowest_price", "median_price", "volume"],
-            });
-            FileSystem.writeFileSync(csvPath, csv);
-        });
+    }
 }
 
 module.exports = writeToCSV;
